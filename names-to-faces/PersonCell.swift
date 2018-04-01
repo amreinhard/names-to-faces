1bf5c9f963e3623782aa56dd401fb2ba0c7a41d4
Amanda <amreinhard@icloud.com>
2018-03-31 23:54:10 -0700

    Completed project, added NSCoding.

---
+++ names-to-faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  names-to-faces
+//
+//  Created by Amanda Reinhard on 3/29/18.
+//  Copyright © 2018 Amanda Reinhard. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel! 
+}
